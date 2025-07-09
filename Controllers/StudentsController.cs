46954bca8a2e5f046b1ddc78f0dd2ee7e4cf26d8
LaibaAsif126 <laibaasif344@gmail.com>
2025-07-09 20:56:35 +0100

    course feature

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -1,11 +1,12 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using AutoMapper;
 using StudentManagement.Data;
+
+
 using StudentManagement.Models;
 
 namespace StudentManagement.Controllers
@@ -13,125 +14,109 @@ namespace StudentManagement.Controllers
     public class StudentsController : Controller
     {
         private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
 
-        public StudentsController(AppDbContext context)
+        public StudentsController(AppDbContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
         // GET: Students
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Students.ToListAsync());
-        }
-
-        // GET: Students/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var students = await _context.Students
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (students == null)
-            {
-                return NotFound();
-            }
-
-            return View(students);
+            var students = await _context.Students.Include(s => s.Course).ToListAsync();
+            var studentDtos = _mapper.Map<List<StudentDto>>(students);
+            return View(studentDtos);
         }
 
         // GET: Students/Create
         public IActionResult Create()
         {
+            ViewData["CourseId"] = new SelectList(_context.Courses, "Id", "Title");
             return View();
         }
 
         // POST: Students/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Email,Department,EnrolledDate")] Students students)
+        public async Task<IActionResult> Create(CreateStudentDto dto)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(students);
+                var student = _mapper.Map<Students>(dto);
+                _context.Add(student);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(students);
+
+            ViewData["CourseId"] = new SelectList(_context.Courses, "Id", "Title", dto.CourseId);
+            return View(dto);
         }
 
         // GET: Students/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
-            {
                 return NotFound();
-            }
 
-            var students = await _context.Students.FindAsync(id);
-            if (students == null)
-            {
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
                 return NotFound();
-            }
-            return View(students);
+
+            var dto = _mapper.Map<CreateStudentDto>(student);
+            ViewData["CourseId"] = new SelectList(_context.Courses, "Id", "Title", dto.CourseId);
+            return View(dto);
         }
 
         // POST: Students/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Email,Department,EnrolledDate")] Students students)
+        public async Task<IActionResult> Edit(int id, CreateStudentDto dto)
         {
-            if (id != students.Id)
+            if (!ModelState.IsValid)
             {
-                return NotFound();
+                ViewData["CourseId"] = new SelectList(_context.Courses, "Id", "Title", dto.CourseId);
+                return View(dto);
             }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(students);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!StudentsExists(students.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(students);
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+                return NotFound();
+
+            _mapper.Map(dto, student);
+            _context.Update(student);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Students/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var student = await _context.Students.Include(s => s.Course).FirstOrDefaultAsync(s => s.Id == id);
+            if (student == null)
+                return NotFound();
+
+            var dto = _mapper.Map<StudentDto>(student);
+            return View(dto);
         }
 
         // GET: Students/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
-            {
                 return NotFound();
-            }
 
-            var students = await _context.Students
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (students == null)
-            {
+            var student = await _context.Students.Include(s => s.Course).FirstOrDefaultAsync(s => s.Id == id);
+            if (student == null)
                 return NotFound();
-            }
 
-            return View(students);
+            var dto = _mapper.Map<StudentDto>(student);
+            return View(dto);
         }
 
         // POST: Students/Delete/5
@@ -139,13 +124,13 @@ namespace StudentManagement.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var students = await _context.Students.FindAsync(id);
-            if (students != null)
+            var student = await _context.Students.FindAsync(id);
+            if (student != null)
             {
-                _context.Students.Remove(students);
+                _context.Students.Remove(student);
+                await _context.SaveChangesAsync();
             }
 
-            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
@@ -155,3 +140,4 @@ namespace StudentManagement.Controllers
         }
     }
 }
+
---
+++ Models/Course.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace StudentManagement.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        public ICollection<Students> Students { get; set; } = new List<Students>();
+    }
+}
+
